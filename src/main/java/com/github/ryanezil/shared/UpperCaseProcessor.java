4ca2e9376978ba12d439dd0f164e60bc40b46835
Rafael Yáñez Illescas <ryanezil@redhat.com>
2022-05-13 10:33:22 +0200

    First commit

---
+++ src/main/java/com/github/ryanezil/shared/UpperCaseProcessor.java
@@ -0,0 +1,16 @@
+package io.ryanezil.shared;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+
+public class UpperCaseProcessor implements Processor {
+
+	@Override
+	public void process(Exchange exchange) throws Exception {
+
+                String body = exchange.getIn().getBody(String.class);
+                body = body.toUpperCase().concat("-nuevo");
+                exchange.getOut().setBody(body);
+	}
+
+}
