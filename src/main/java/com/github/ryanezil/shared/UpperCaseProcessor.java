0d81cf5c56fde07e61ba110e71600e2af9a5e722
Rafael Yáñez Illescas <ryanezil@redhat.com>
2022-06-03 18:16:23 +0200

    Added annotation @ApplicationScoped to processor

--- src/main/java/com/github/ryanezil/shared/UpperCaseProcessor.java
+++ src/main/java/com/github/ryanezil/shared/UpperCaseProcessor.java
@@ -3,13 +3,16 @@ package com.github.ryanezil.shared;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
 public class UpperCaseProcessor implements Processor {
 
 	@Override
 	public void process(Exchange exchange) throws Exception {
 
                 String body = exchange.getIn().getBody(String.class);
-                body = body.toUpperCase().concat("-github-fix");
+                body = body.toUpperCase().concat("-mytext");
                 exchange.getOut().setBody(body);
 	}
 
